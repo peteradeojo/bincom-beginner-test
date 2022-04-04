1409b30c9ad5113a555996a5b17b790f2b0d8a78
Peter Ade-Ojo <peteradeojo@outlook.com>
2022-04-04 19:21:14 +0100

    deployment setup still

--- Q2/todo_list/settings.py
+++ Q2/todo_list/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,3 +135,8 @@ LOGIN_URL = 'users:login'
 # Heroku Settings
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
