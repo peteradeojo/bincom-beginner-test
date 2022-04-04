12b256bc901b7f2095f82ef9f6275e5eed6c262f
Peter Ade-Ojo <peteradeojo@outlook.com>
2022-04-04 19:09:56 +0100

    setup for heroku deployment

--- Q2/todo_list/settings.py
+++ Q2/todo_list/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'users:login'
+
+
+# Heroku Settings
+import django_heroku
+django_heroku.settings(locals())
